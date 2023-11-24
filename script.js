5fc698995d977501cd9360fe46b35577920d447a
Alan <alanpeyer@gmail.com>
2023-11-24 10:00:08 +0100

    revisit code, create a function to create dinamically a square grid, added some cs for visual implementation

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const gridContainer = document.querySelector('container');
+const gridContainer = document.querySelector('#container');
+let gridElement = 16;
+function makeRow (){
+    for(let i = 0; i < Math.pow(gridElement, 2); i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        gridContainer.appendChild(cell);
+    }
+}
 
-const grid = document.createElement('div');
-
-gridContainer.appendChild(grid);
-
-grid.classList.add('grid');
+makeRow();
