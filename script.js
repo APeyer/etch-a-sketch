7e2f8c107e0933b4041807ae8e69ea9ea1d26a12
Alan <alanpeyer@gmail.com>
2023-11-29 19:05:23 +0100

    added a button to change the number of cells in the grid with an input

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 const gridContainer = document.querySelector('#container');
-let gridElement = 16;
+
 let cell = '';
+let size = 16;
+
+let cells = document.querySelectorAll('.cell');
 
 
 function makeGrid (){
-    for(let i = 0; i < Math.pow(gridElement, 2); i++){
+    for(let i = 0; i < Math.pow(size, 2); i++){
         cell = document.createElement('div');
         cell.classList.add('cell');
         gridContainer.appendChild(cell);
     }
+    cellToBlack();
 }
 
 makeGrid();
 
-let cells = document.querySelectorAll('.cell');
-
-cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.setProperty("background-color","black");
-    });
+document.querySelector('#gridSize').addEventListener('click', () => {
+    size = prompt('Choose grid size');
+    if (size <= 100){
+        reset();
+        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        makeGrid(size);
+        cellToBlack();
+    } else {
+        alert('The maximum amount of cells are 100, try less')
+    }
+    
 });
 
+function cellToBlack (){
+    cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.setProperty("background-color","black");
+        });
+    });
+}
+
+function reset() {
+    cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        gridContainer.removeChild(cell);
+    });
+}
